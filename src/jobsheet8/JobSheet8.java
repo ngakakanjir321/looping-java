6d73d9c5fe6063eb9d10fa651e7e2bb51df3835e
p tanto <p tanto@DESKTOP-UDJ7LRS>
2018-08-02 11:34:35 +0700

    for, while, do while

---
+++ src/jobsheet8/JobSheet8.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet8;
+
+/**
+ *
+ * @author p tanto
+ */
+public class JobSheet8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+              int x;
+        for (x=1;x<=5;x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        
+        int y = 1;
+        while (y <= 5)
+        {
+            System.out.println("Smk telkom malang");
+            y++;
+        }
+        
+        int z = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            z++;
+        }
+        while (z <= 5);
+        
+    }
+}
+    
+    
